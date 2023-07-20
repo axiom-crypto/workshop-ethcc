d4810fb4d5c105850cba2b2cbe0420ea7037e0eb
Yu Jiang Tham <yoojbruin@gmail.com>
2023-07-21 01:40:13 +0200

    Deploy goerli script

--- contracts/script/local/deploy_goerli.sh
+++ contracts/script/local/deploy_goerli.sh
@@ -1,2 +1,2 @@
 source .env
-forge script script/Distributor.s.sol:DistributorScript --private-key $PRIVATE_KEY --broadcast --rpc-url $PROVIDER_URI_GOERLI -vvvv
+forge script script/Distributor.s.sol:DistributorScript --private-key $PRIVATE_KEY --broadcast --rpc-url $PROVIDER_URI_GOERLI -vvvv && cp out/Distributor.sol/Distributor.json ../webapp/src/shared/abi/Distributor.json
